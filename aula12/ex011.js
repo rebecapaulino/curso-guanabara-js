1a3df04c4340c7f8c1de4a4ae76a73ae1f45ec84
Rebeca Oliveira <rebecaoliver2000@gmail.com>
2025-11-06 15:57:03 -0300

    This reverts commit 82b4623baffbf533864bf1ac031946935ad7b2d3.

--- aula12/ex011.js
+++
@@ -1,9 +0,0 @@
-var idade = 12
-console.log(`Você tem ${idade} anos`)
-if (idade < 16) {
-        console.log(`Você não vota`)
-}   else if (idade < 18 || idade > 65) {
-        console.loge(`Seu voto opcional`)
-}   else {
-        console.log(`Seu voto obrigatório`)
-}
